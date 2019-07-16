3b6e20c590e57fdfe9fd3c7c0f6b37242cd331eb
Angela <angela.liu1399@gmail.com>
2019-07-16 14:41:03 -0700

    Add picture for background; generate textView

--- app/src/main/java/com/example/waves_app/fragments/FishTankFragment.java
+++ app/src/main/java/com/example/waves_app/fragments/FishTankFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,6 +13,10 @@ import androidx.fragment.app.Fragment;
 import com.example.waves_app.R;
 
 public class FishTankFragment extends Fragment {
+
+    public static final String TAG = "FishTankFragment";
+    private TextView tvFishCount;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -20,6 +25,6 @@ public class FishTankFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        tvFishCount = (TextView) view.findViewById(R.id.tvFishCount);
     }
 }
