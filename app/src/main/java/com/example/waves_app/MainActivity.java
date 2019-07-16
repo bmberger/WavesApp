6b3316159fa3499f8407c447e7bc005d3b57c4f9
Briana Berger <brianamb@fb.com>
2019-07-16 13:24:06 -0700

    Set up the navigation bar and basic structure for fragments

--- app/src/main/java/com/example/waves_app/MainActivity.java
+++ app/src/main/java/com/example/waves_app/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.waves_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.example.waves_app.fragments.CalendarFragment;
+import com.example.waves_app.fragments.FishTankFragment;
+import com.example.waves_app.fragments.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+// Home page
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+
+                switch (item.getItemId()) {
+                    case R.id.miHome:
+                        fragment = new HomeFragment();
+                        Toast.makeText(MainActivity.this, "Switched to home!", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id. miCalendar:
+                        fragment = new CalendarFragment();
+                        Toast.makeText(MainActivity.this, "Switched to cal!", Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.miFishTank:
+                        fragment = new FishTankFragment();
+                        Toast.makeText(MainActivity.this, "Switched to fish!", Toast.LENGTH_LONG).show();
+                        break;
+                    default:
+                        return true;
+                }
+
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.miHome);
     }
 }
