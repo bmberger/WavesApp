93e1e0069a8520f4b15045f9caf1c4001c7c8ad2
Angela <angela.liu1399@gmail.com>
2019-07-18 13:31:32 -0700

    Add Category class

---
+++ app/src/main/java/com/example/waves_app/model/Category.java
@@ -0,0 +1,31 @@
+package com.example.waves_app.model;
+
+import java.util.List;
+
+public class Category {
+
+    private String categoryName;
+    private int categorySize;
+    private List<Task> tasks;
+
+    public Category() { }
+
+    // Getter methods
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public int getCategorySize() {
+        // TODO - read lines or something from user persistence file
+        return categorySize;
+    }
+
+    // Setter methods
+    public void setCategoryName(String name) {
+        categoryName = name;
+    }
+
+    public void setCategorySize() {
+        // TODO - READ THROUGH THE FILE FROM USER PERSISTENCE
+    }
+}
