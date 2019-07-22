48ba321931fffd5ce8fbdfdf4bb9bba73b950081
Briana Berger <brianamb@fb.com>
2019-07-22 14:32:57 -0700

    Registering and creating BroadcastReciever code

--- app/src/main/java/com/example/waves_app/MyAlarm.java
+++ app/src/main/java/com/example/waves_app/MyAlarm.java
@@ -1,4 +1,20 @@
 package com.example.waves_app;
 
-public class MyAlarm {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+//class extending the Broadcast Receiver
+public class MyAlarm extends BroadcastReceiver {
+
+    //the method will be fired when the alarm is triggerred
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        // you can check the log to see if it is fired - you can do any task here
+        Log.d("MyAlarm", "Alarm just fired");
+    }
+
 }
