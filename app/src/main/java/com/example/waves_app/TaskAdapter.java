805408c7354d103212128852422fe5608935c11d
Briana Berger <brianamb@fb.com>
2019-07-22 23:31:14 -0700

    Enabled uniquely identifying each notification and enabled removing/checking off with notifications

--- app/src/main/java/com/example/waves_app/TaskAdapter.java
+++ app/src/main/java/com/example/waves_app/TaskAdapter.java
@@ -249,6 +249,7 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
                             new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int id) {
                                     pos = getAdapterPosition();
+                                    cancelAlarm(mTasksList.get(pos).getTaskDetail());
                                     mTasksList.remove(pos);
                                     parsedData.remove(pos);
                                     writeTaskItems();
@@ -281,7 +282,7 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
         }
     }
 
-    //TODO: to be called in adding a task (for both due date AND task detail/desc)(JUST ADDING THOUGH)
+    // to be called in adding a task (for both due date AND task detail/desc)(JUST ADDING THOUGH)
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void setAlarm(String dueDate, String taskDetail) {
         // For adding/setting a new alarm
@@ -304,31 +305,44 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
         // has the alarm go off at 7pm on user's set date
         calendar.setTimeInMillis(System.currentTimeMillis());
         calendar.clear();
-        calendar.set(year,month - 1,dayOfMonth,18,03); //04:15pm 18:00 is for 7pm
+        calendar.set(year,month - 1,dayOfMonth,23,27); //04:15pm 19:00 is for 7pm
 
         // allows us to utilize broadcasting and alarms
         Intent myIntent = new Intent(this.context, MyAlarm.class);
         myIntent.putExtra("taskDetail", taskDetail);
 
+        // takes a task and returns a unique id - utilized to keep track of different alarms (adding, deleting, editing)
+        int id = taskDetail.hashCode();
+
         // for others to understand a bit better: https://medium.com/@architgupta690/creating-pending-intent-in-android-a-step-by-step-guide-74784ec60c9e
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this.context, 0, myIntent, 0);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this.context, id, myIntent, 0);
 
         // sets up the actual alarm
         alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-        Toast.makeText(this.context, "Alarm is set", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this.context, "Alarm has been set", Toast.LENGTH_SHORT).show();
         Log.d("TaskAdapter", "Alarm set");
     }
 
-    //TODO: to be called in removing a task and in checking off a task
-    public static void cancelAlarm() {
+    // to be called in removing a task and in checking off a task
+    public void cancelAlarm(String taskDetail) {
         // For canceling an alarm
 
+        // allows us to utilize broadcasting and alarms
+        Intent myIntent = new Intent(this.context, MyAlarm.class);
+        myIntent.putExtra("taskDetail", taskDetail);
+
+        int id = taskDetail.hashCode();
+
+        // for others to understand a bit better: https://medium.com/@architgupta690/creating-pending-intent-in-android-a-step-by-step-guide-74784ec60c9e
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this.context, id, myIntent, 0);
+
+        alarmManager.cancel(pendingIntent);
+        Log.d("TaskAdapter", "Alarm canceled");
     }
 
     //TODO: to be called in editing a task (for both due date AND task detail/desc) (JUST EDITING THOUGH)
-    public static void editAlarm() {
+    public void editAlarm() {
         // For editing an alarm
 
     }
-
 }
