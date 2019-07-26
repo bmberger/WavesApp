11c95f93e4c11d1c1dae33bef60bf2213b572aa4
Briana Berger <brianamb@fb.com>
2019-07-26 16:10:01 -0700

    Created a sand UI on settings and tutorial screen

--- app/src/main/java/com/example/waves_app/fragments/TutorialFragment.java
+++ app/src/main/java/com/example/waves_app/fragments/TutorialFragment.java
@@ -15,7 +15,9 @@ public class TutorialFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tutorial, container, false);
+        View view = inflater.inflate(R.layout.fragment_tutorial, container, false);
+        view.setBackgroundDrawable(getResources().getDrawable(R.drawable.sand_background));
+        return view;
     }
 
     @Override
