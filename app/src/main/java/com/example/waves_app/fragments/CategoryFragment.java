1e82f240e29aed1f8d572f07092f2d1c58450f35
Briana Berger <brianamb@fb.com>
2019-07-26 16:07:26 -0700

    Created a sand UI on category screen

--- app/src/main/java/com/example/waves_app/fragments/CategoryFragment.java
+++ app/src/main/java/com/example/waves_app/fragments/CategoryFragment.java
@@ -72,7 +72,9 @@ public class CategoryFragment extends Fragment implements OnStartDragListener {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_categories, container, false);
+        View view = inflater.inflate(R.layout.fragment_categories, container, false);
+        view.setBackgroundDrawable(getResources().getDrawable(R.drawable.sand_background));
+        return view;
     }
 
     @Override
