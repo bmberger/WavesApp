5a17d30b2f7e0f9c9239e29880fe9076e1d80fb5
Briana Berger <brianamb@fb.com>
2019-08-05 13:05:33 -0700

    Created splash/launch screen for users to see when app is loading

---
+++ app/src/main/java/com/example/waves_app/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.example.waves_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        // Creates splash/launch screen for user
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
