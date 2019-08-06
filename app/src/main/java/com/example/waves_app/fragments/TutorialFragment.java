9dea5fc86bb1c0f7a063f646920ebc8db1ef8359
Angela <angela.liu1399@gmail.com>
2019-08-06 11:46:07 -0700

    Fix typo and grammar in tutorial cards

--- app/src/main/java/com/example/waves_app/fragments/TutorialFragment.java
+++ app/src/main/java/com/example/waves_app/fragments/TutorialFragment.java
@@ -46,12 +46,12 @@ public class TutorialFragment extends Fragment {
         tutorials.add("Swipe up and down on the screen to navigate between the home page, calendar, and fish tank.");
         tutorials.add("Press the back button to return to the home page.");
         tutorials.add("To add a new category or task, tap below the current list and we'll generate one for you!");
-        tutorials.add("Swipe left on a category or task to delete the selected item.");
-        tutorials.add("Swipe right on a task to mark it as complete");
-        tutorials.add("Check your fish tank to see how much fish you've saved!");
+        tutorials.add("Swipe to the left on a category or task to delete the selected item.");
+        tutorials.add("Swipe to the right on a task to mark it as complete.");
         tutorials.add("If you forget where a task is located or when it's due, go to the search option and type in what you remember of the task.");
         tutorials.add("Open the share option to send a particular list of tasks to others by email!");
         tutorials.add("Tap into the facts option to learn fun facts about oceans.");
+        tutorials.add("Check your fish tank to see how much fish you've saved!");
         tutorials.add("Look into our FAQ to find out how you can make an impact and our story.");
 
         // Create new adapter for tutorial steps
