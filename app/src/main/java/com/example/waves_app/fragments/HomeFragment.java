ee1432231c2cccd58915518a0e30225d03e726d7
Angela <angela.liu1399@gmail.com>
2019-08-08 15:40:25 -0700

    Remove Parse classes/layouts

--- app/src/main/java/com/example/waves_app/fragments/HomeFragment.java
+++ app/src/main/java/com/example/waves_app/fragments/HomeFragment.java
@@ -8,12 +8,8 @@
 
 package com.example.waves_app.fragments;
 
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -22,7 +18,6 @@ import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -37,7 +32,6 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 public class HomeFragment extends Fragment {
 
